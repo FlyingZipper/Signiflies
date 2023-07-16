c877e8c5549e3ec55d6d0bc01da14d3e357e6975
Philippe Trudel <p.trudel1996@outlouk.com>
2023-07-15 23:49:31 -0400

    fix: removed redondant pages

--- src/app/games/pages.tsx
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import { useRouter } from 'next/router'
-import React from 'react'
-import { redirect } from 'next/navigation';
-
-export default async function Games({ params }: any) {
-    redirect('/');
-}
